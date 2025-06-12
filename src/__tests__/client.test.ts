364e43e43d035d8af37dafdab0d57333f91a9776
George Alexiou <george@alexiou.xyz>
2025-06-12 12:24:27 +0100

    chore: update dependencies and configure Jest for testing

---
+++ src/__tests__/client.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from '@jest/globals';
+
+describe('example', () => {
+
+  describe('example', () => {
+
+    it('example', async () => {
+      expect(true).toBe(true);
+    });
+  });
+}); 
